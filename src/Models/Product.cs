c912fd0942e7824e840208e85fae2ae8be37f17f
bxnjadev <benjamin.hyx@gmail.com>
2024-09-07 00:31:23 -0400

    refactor: created a product type enum for classifier types

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -5,12 +5,22 @@ using System.Threading.Tasks;
 
 namespace Taller1.src.Models
 {
+
+    public enum ProductType
+    {
+        TShirt,
+        Cap,
+        Toy,
+        Food,
+        Book
+    }
+
     public class Product
     {
         public int Id {get;set;}
         public string Name {get;set;} = string.Empty;
 
-        public string Kind {get;set;} = string.Empty;
+        public ProductType ProductType {get;set;} = string.Empty;
 
         public int Price {get;set;}
 

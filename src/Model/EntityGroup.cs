a71b00731d28627ca8f3f1b9f321c5fffef6be48
bxnjadev <benjamin.hyx@gmail.com>
2024-09-29 12:28:42 -0300

    docs: documented EntityGroup

--- src/Model/EntityGroup.cs
+++ src/Model/EntityGroup.cs
@@ -1,11 +1,22 @@
 ﻿namespace Taller1.Model;
 
+/// <summary>
+/// This class represent a collection of elements for present 
+/// </summary>
+/// <typeparam name="T">The type object</typeparam>
+
 public class EntityGroup<T>
 {
-
+    
     private readonly IEnumerable<T> _entities;
     private readonly Dictionary<string, string> _metadata;
 
+    /// <summary>
+    /// This is main constructor for create a entity group
+    /// </summary>
+    /// <param name="entities">A collection of entities</param>
+    /// <param name="metadata">A metadata from collection</param>
+    
     public EntityGroup(
         IEnumerable<T> entities,
         Dictionary<string, string> metadata
@@ -15,11 +26,22 @@ public class EntityGroup<T>
         _metadata = metadata;
     }
 
+    /// <summary>
+    /// Retrieve a collection from entities
+    /// </summary>
+    /// <returns>A collection from entities</returns>
+    
     public IEnumerable<T> GetEntities()
     {
         return _entities;
     }
-
+    
+    /// <summary>
+    /// Retrieve a dictionary that contains options and metadata
+    /// in a format key-value
+    /// </summary>
+    /// <returns>A dictionary with metadata about the entities</returns>
+    
     public Dictionary<string, string> GetMetadata()
     {
         return _metadata;

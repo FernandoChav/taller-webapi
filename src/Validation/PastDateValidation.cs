3d32403f75ee8fa9bf249bc72db9b011fed1b33a
bxnjadev <benjamin.hyx@gmail.com>
2024-10-16 14:35:03 -0300

    docs: added documentation for package validation

--- src/Validation/PastDateValidation.cs
+++ src/Validation/PastDateValidation.cs
@@ -2,10 +2,21 @@
 
 namespace Taller1.Validation;
 
+/// <summary>
+/// This is implementation for validate if a date is past that this actual date
+/// </summary>
+
 [AttributeUsage(AttributeTargets.Property |
                 AttributeTargets.Field, AllowMultiple = false)]
 public class PastDateValidation : ValidationAttribute
 {
+    
+    /// <summary>
+    /// Check if a date is elder that actual
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    
     public override bool IsValid(object? value)
     {
         if (value == null)
@@ -17,6 +28,12 @@ public class PastDateValidation : ValidationAttribute
         return date < DateTime.Now;
     }
 
+    /// <summary>
+    /// Show a message error
+    /// </summary>
+    /// <param name="name">Name attribute</param>
+    /// <returns></returns>
+    
     public override string FormatErrorMessage(string name)
     {
         return $"Error: {name}";
--- src/Validation/RutValidator.cs
+++ src/Validation/RutValidator.cs
@@ -2,11 +2,21 @@
 
 namespace Taller1.Validation;
 
+/// <summary>
+/// This is a implementation for validate is a rut is valid
+/// </summary>
+
 [AttributeUsage(AttributeTargets.Property |
                 AttributeTargets.Field, AllowMultiple = false)]
 public class RutValidator : ValidationAttribute
 {
 
+    /// <summary>
+    /// Check if a rut is valid
+    /// </summary>
+    /// <param name="rutAsObject"> The rut as object can be null </param>
+    /// <returns></returns>
+    
     public override bool IsValid(object? rutAsObject)
     {
         if (rutAsObject == null)
@@ -18,6 +28,12 @@ public class RutValidator : ValidationAttribute
 
         return checkIfIsARutValid(rut);
     }
+    
+    /// <summary>
+    /// Check if a rut is valid
+    /// </summary>
+    /// <param name="rutAsString"> The rut as St ring </param>
+    /// <returns></returns>
 
     public bool checkIfIsARutValid(string rutAsString)
     {
